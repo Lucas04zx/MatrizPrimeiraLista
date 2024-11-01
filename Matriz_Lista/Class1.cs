8c34ebda292ae1baf9b6481bfffdf6893c88167b
LUCAS ANTONIO FERREIRA NETO <15541535654@CAMPUS.MACHADO.MCH>
2024-11-01 09:15:18 -0300

    Primeira Lista

---
+++ Matriz_Lista/Class1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+
+namespace Biblioteca_Matriz
+{
+    class Matriz
+    {
+        public static void leia(int[ , ] mat)
+        {
+            int Linhas = mat.GetLength( 0 );
+            int Coluna = mat.GetLength(1);
+
+            for (int i = 0; i < Linhas; i++)
+            {
+                for (int j = 0; j < Coluna; j++)
+                {
+                    Console.Write($"Escreva o valor de : [ {i} , {j} ] : ");
+                    mat[i, j]= int.Parse(Console.ReadLine()); 
+                    
+                }   
+            }//Fim dos for
+
+        }//Fim leia
+
+        public static void mostra(int[,] mat)
+        {
+            int Linha = mat.GetLength( 0 );
+            int Coluna = mat.GetLength(1);
+            for(int i = 0;i < Linha; i++)
+            {
+                for(int j = 0;j < Coluna; j++)
+                {
+                    Console.Write($"|{mat[i,j],3}| ");
+                }//fim for j
+                Console.WriteLine();
+            }//fim for I
+        }//Fim mostre
+        public static void gera(int[,] mat)
+        {
+            int Linha = mat.GetLength(0);
+            int Coluna = mat.GetLength(1);
+            Random random = new Random();
+            for (int i = 0; i < Linha; i++)
+            
+                
+                for (int j = 0; j < Coluna; j++)
+                
+                    mat[i, j] = random.Next(1, 50);
+                
+                Console.WriteLine();
+            //fim for I
+        }//Fim mostre
+
+    }//Fim gera
+    }//Classe matriz
+//Fim biblioteca
---
+++ Matriz_Lista/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Biblioteca_Matriz;
+
+class Program
+{
+   static void Main()
+    {
+        int linhas, colunas;
+
+        Console.WriteLine("== Matrizes ==");
+
+        Console.WriteLine("Digites os dados da matriz, indique nesta ordem: ");
+
+        Console.Write("Linhas: ");
+        linhas = int.Parse(Console.ReadLine());
+
+        Console.WriteLine();
+
+        Console.Write("Colunas: ");
+        colunas = int.Parse(Console.ReadLine());
+
+        int[ , ] mat = new int[linhas, colunas];
+
+        Console.WriteLine("Agora insira os dados da matriz");
+        Matriz.gera(mat);
+
+        Console.WriteLine("A Matriz é:");
+        Matriz.mostra(mat);
+
+        Console.ReadKey();
+    }
+}
